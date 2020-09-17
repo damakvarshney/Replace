73c97d173de3d4f9a18798d6f42639efc0201d80
damakvarshney <damakvarshney@gmail.com>
2020-09-17 23:47:27 +0530

    17/9/20

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,7 @@ import cache from "../utility/cache";
 import authStorage from "../auth/storage";
 import settings from "../config/settings";
 const client = create({
-  baseURL: "http://192.168.42.162:9000/api",
+  baseURL: "http://192.168.42.203:9000/api",
 });
 
 client.addAsyncRequestTransform(async (request) => {
